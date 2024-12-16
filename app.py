fdb482f6645244dd7f65dbf9dba71432c075f06c
Kurt DeMaagd <kurt@sightmachine.com>
2024-12-16 10:33:09 -0500

    change api for model catalog format

--- app.py
+++ app.py
@@ -5,11 +5,79 @@ import csv
 import json
 from flask import Flask, jsonify, render_template, request, Response
 
-from azure.ai.inference import ChatCompletionsClient
-from azure.core.credentials import AzureKeyCredential
-from azure.ai.inference.models import SystemMessage, AssistantMessage, UserMessage
+import urllib.request
+import json
+import os
+import ssl
 
 app = Flask(__name__)
+ENDPOINT = ""
+API_KEY = ""
+
+
+def get_name_from_examples(examples, instructions, old, endpoint, api_key):
+    def allowSelfSignedHttps(allowed):
+        # bypass the server certificate verification on client side
+        if (
+            allowed
+            and not os.environ.get("PYTHONHTTPSVERIFY", "")
+            and getattr(ssl, "_create_unverified_context", None)
+        ):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+    system_prompt = f""" 
+    You are an AI companion creates renaming rules for variable names.
+    
+    Here are some hints to help you with the renaming:
+    - If you see a number, followed by a dash, followed by a number -- e.g., 1-3 -- these are important parts of the naming.
+    - In general, pay attention to numbers.  They are often important.
+    
+    A typical name will have a pattern like Line#_MachineName_Sensor.
+    
+    The following examples are associated with predefined mappings:
+    {examples}
+
+    The following are additional instructions on performing the renaming:
+    {instructions}
+    """
+
+    user_prompt = f""" 
+    What is the best name associated with keywords '{old}'?
+    """
+
+    allowSelfSignedHttps(
+        True
+    )  # this line is needed if you use self-signed certificate in your scoring service.
+
+    data = {
+        "input_data": {
+            "input_string": [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            "parameters": {"temperature": 0.0},
+        }
+    }
+
+    body = str.encode(json.dumps(data))
+    url = endpoint
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": ("Bearer " + api_key),
+    }
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read()
+        return json.loads(result)["output"].strip()
+
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        print(error.info())
+        print(error.read().decode("utf8", "ignore"))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -43,57 +111,22 @@ def generate_table():
     user_input_new_tags = data.get("rename_tags", "")
     user_input_instructions = data.get("additional_info", "")
 
-    client = ChatCompletionsClient(
-        endpoint=endpoint,
-        credential=AzureKeyCredential(api_key),
-        max_tokens=1000,
-        temperature=0,
-    )
-
-    user_pre_prompt = f""" 
-    You are an AI companion that uses this data to try to predict new names based on provided keywords.
-    If you see a number, followed by a dash, followed by a number -- e.g., 1-3 -- these are important parts of the naming.
-    In general, pay attention to numbers.  They are often important.
-
-    The following keywords are associated with predefined mappings:
-    {user_input_examples}
-
-    {user_input_instructions}
-
-    A typical name will have a city name followed by a line number followed by a machine followed by sensor or measurement specific information.
-    Common machine names are BLENDER, PALLETIZER, FILLER, etc.  
-    For example 
-    - DET_L1_BLENDER_TEMP 
-    - WAS_L3_PALLETIZER_LAYHNDL 
-    - SAN_L10_FILLER_PRESS3
-    
-    
-    """
-
-    assist_reply = "OK, I will assist you with that."
     table_data = []
-
     for new_tag_info in user_input_new_tags.split("\n"):
         if not new_tag_info:
             continue
         print(f"For {new_tag_info}")
-        user_prompt = f""" 
-        What is the best name associated with keywords '{new_tag_info}'?
-        """
 
         try:
-            response = client.complete(
-                messages=[
-                    # UserMessage(content=user_pre_prompt),
-                    # AssistantMessage(content=assist_reply),
-                    SystemMessage(content=user_pre_prompt),
-                    UserMessage(content=user_prompt),
-                ],
+            tag_new_name = get_name_from_examples(
+                user_input_examples,
+                user_input_instructions,
+                new_tag_info,
+                endpoint,
+                api_key,
             )
-
-            tag_new_name = response["choices"][0]["message"]["content"]
-            tag_new_name = tag_new_name.strip()
             print(f"Answer is {tag_new_name}")
+
             table_data.append(
                 {"input_definition": new_tag_info, "new_name": tag_new_name}
             )
